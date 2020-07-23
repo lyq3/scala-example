ccba5e98e4543e2dd5b2dc26da6f96df3edcfeee
卡卢比 <luoyunqiang@ypzdw.com>
2020-07-23 11:16:48 +0800

    方法学习

--- src/com/lyq3/scala/method/MethodExample.scala
+++ src/com/lyq3/scala/method/MethodExample.scala
@@ -24,6 +24,26 @@ class MethodExample {
     */
   def plusOne(i : Int) = i + 1
 
+  /**
+    * 默认值
+    *
+    * 调用：参数的复制顺序是从左到右的
+    * obj.defaultValueMethod()
+    * obj.defaultValueMethod("尼古拉斯佩奇")
+    * obj.defaultValueMethod("尼古拉斯佩奇",30)
+    * obj.defaultValueMethod(age = 26)
+    * @param age
+    * @return
+    */
+  def defaultValueMethod(name : String = "卡卢比",age : Int = 18): Int = {
+      return age + 1
+  }
+
+
+
+
+
+
 
 
 
